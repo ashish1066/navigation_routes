39caa4e6b85515b3a9564fba850e6361c0515a41
Ashish <ashish@theflyy.com>
2022-03-24 10:30:52 +0530

    jitsi removed

--- navigation_and_routing/lib/src/screens/authors.dart
+++ navigation_and_routing/lib/src/screens/authors.dart
@@ -4,11 +4,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flyy_flutter_plugin/flyy_flutter_plugin.dart';
-import 'package:jitsi_meet/feature_flag/feature_flag.dart';
-import 'package:jitsi_meet/jitsi_meet.dart';
 
 import '../data/library.dart';
-import '../routing.dart';
 import '../widgets/author_list.dart';
 
 class AuthorsScreen extends StatelessWidget {
@@ -24,32 +21,10 @@ class AuthorsScreen extends StatelessWidget {
         body: AuthorList(
           authors: libraryInstance.allAuthors,
           onTap: (author)  {
-             joinMeeting();
-           // FlyyFlutterPlugin.openFlyyOffersPage();
+            FlyyFlutterPlugin.openFlyyOffersPage();
             //RouteStateScope.of(context).go('/author/${author.id}');
           },
         ),
       );
 
-  void joinMeeting() async {
-    try {
-      FeatureFlag featureFlag = FeatureFlag();
-      featureFlag.welcomePageEnabled = false;
-      featureFlag.resolution = FeatureFlagVideoResolution.MD_RESOLUTION; // Limit video resolution to 360p
-
-      var options = JitsiMeetingOptions(room: 'myroom1212322') // Required, spaces will be trimmed
-        ..serverURL = "https://meet.jit.si"
-        ..subject = "Meeting with Gunschu"
-        ..userDisplayName = "My Name"
-        ..userEmail = "myemail@email12234.com"
-        ..userAvatarURL = "https://images.unsplash.com/photo-1604085572504-a392ddf0d86a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8b3JhbmdlJTIwZmxvd2VyfGVufDB8fDB8fA%3D%3D&w=1000&q=80" // or .png
-        ..audioOnly = true
-        ..audioMuted = true
-        ..videoMuted = true;
-
-      await JitsiMeet.joinMeeting(options);
-    } catch (error) {
-      debugPrint("error: $error");
-    }
-  }
 }
